64591677e6ecc92cce4d839cf72ce307ac482662
DiaDeMuertos <imworkingnow@msn.com>
2020-02-22 02:17:56 -0700

    added CRUD

--- amplify/backend/function/apia/src/app.js
+++ amplify/backend/function/apia/src/app.js
@@ -1,91 +1,74 @@
-/*
-Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-    http://aws.amazon.com/apache2.0/
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-*/
-
-
-
-const AWS = require('aws-sdk')
-var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
-var bodyParser = require('body-parser')
-var express = require('express')
+const AWS = require('aws-sdk');
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+var bodyParser = require('body-parser');
+var express = require('express');
+const uuid = require('uuid/v1');
 
 AWS.config.update({ region: process.env.TABLE_REGION });
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-let tableName = "items";
-if(process.env.ENV && process.env.ENV !== "NONE") {
+let tableName = 'items';
+if (process.env.ENV && process.env.ENV !== 'NONE') {
   tableName = tableName + '-' + process.env.ENV;
 }
 
 const userIdPresent = false; // TODO: update in case is required to use that definition
-const partitionKeyName = "itemId";
-const partitionKeyType = "S";
-const sortKeyName = "";
-const sortKeyType = "";
-const hasSortKey = sortKeyName !== "";
-const path = "/items";
+const partitionKeyName = 'itemId';
+const partitionKeyType = 'S';
+const sortKeyName = '';
+const sortKeyType = '';
+const hasSortKey = sortKeyName !== '';
+const path = '/items';
 const UNAUTH = 'UNAUTH';
 const hashKeyPath = '/:' + partitionKeyName;
 const sortKeyPath = hasSortKey ? '/:' + sortKeyName : '';
-// declare a new express app
-var app = express()
-app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
 
-// Enable CORS for all methods
+var app = express();
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
+
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
-// convert url string param to expected Type
-const convertUrlType = (param, type) => {
-  switch(type) {
-    case "N":
-      return Number.parseInt(param);
-    default:
-      return param;
-  }
-}
+/************************************
+ * HTTP post method for insert object *
+ *************************************/
+
+app.post(path, function(req, res) {
+  const item = { ...req.body, itemId: uuid() };
+  const queryParams = {
+    TableName: tableName,
+    Item: item,
+  };
+  dynamodb.put(queryParams, err => {
+    if (err) {
+      res.statusCode = 500;
+      res.json({ error: err, url: req.url, body: req.body });
+    } else {
+      res.json({ data: item });
+    }
+  });
+});
 
 /********************************
  * HTTP Get method for list objects *
  ********************************/
 
-app.get(path + hashKeyPath, function(req, res) {
-  var condition = {}
-  condition[partitionKeyName] = {
-    ComparisonOperator: 'EQ'
-  }
-
-  if (userIdPresent && req.apiGateway) {
-    condition[partitionKeyName]['AttributeValueList'] = [req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH ];
-  } else {
-    try {
-      condition[partitionKeyName]['AttributeValueList'] = [ convertUrlType(req.params[partitionKeyName], partitionKeyType) ];
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-
-  let queryParams = {
+app.get(path, function(req, res) {
+  const queryParams = {
     TableName: tableName,
-    KeyConditions: condition
-  }
+  };
 
-  dynamodb.query(queryParams, (err, data) => {
+  dynamodb.scan(queryParams, (err, data) => {
     if (err) {
       res.statusCode = 500;
-      res.json({error: 'Could not load items: ' + err});
+      res.json({ error: 'Could not load items: ' + err });
     } else {
-      res.json(data.Items);
+      res.json({ data: data.Items });
     }
   });
 });
@@ -94,140 +77,78 @@ app.get(path + hashKeyPath, function(req, res) {
  * HTTP Get method for get single object *
  *****************************************/
 
-app.get(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
-  var params = {};
-  if (userIdPresent && req.apiGateway) {
-    params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  } else {
-    params[partitionKeyName] = req.params[partitionKeyName];
-    try {
-      params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-  if (hasSortKey) {
-    try {
-      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
+app.get(path + '/:itemId', function(req, res) {
+  const { itemId } = req.params;
 
-  let getItemParams = {
+  const queryParams = {
     TableName: tableName,
-    Key: params
-  }
+    Key: {
+      itemId: itemId,
+    },
+    KeyConditionExpression: 'itemId=:itemId',
+    ExpressionAttributeValues: {
+      ':itemId': itemId,
+    },
+  };
 
-  dynamodb.get(getItemParams,(err, data) => {
-    if(err) {
+  dynamodb.query(queryParams, (err, data) => {
+    if (err) {
       res.statusCode = 500;
-      res.json({error: 'Could not load items: ' + err.message});
+      res.json({ error: 'Could not load items: ' + err.message });
     } else {
-      if (data.Item) {
-        res.json(data.Item);
-      } else {
-        res.json(data) ;
-      }
+      res.json({ data: data.Items });
     }
   });
 });
 
-
 /************************************
-* HTTP put method for insert object *
-*************************************/
+ * HTTP put method for insert object *
+ *************************************/
 
 app.put(path, function(req, res) {
+  const item = { ...req.body };
 
-  if (userIdPresent) {
-    req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  }
-
-  let putItemParams = {
+  const queryParams = {
     TableName: tableName,
-    Item: req.body
-  }
-  dynamodb.put(putItemParams, (err, data) => {
-    if(err) {
+    Item: item,
+  };
+
+  dynamodb.put(queryParams, (err, data) => {
+    if (err) {
       res.statusCode = 500;
-      res.json({error: err, url: req.url, body: req.body});
-    } else{
-      res.json({success: 'put call succeed!', url: req.url, data: data})
-    }
-  });
-});
-
-/************************************
-* HTTP post method for insert object *
-*************************************/
-
-app.post(path, function(req, res) {
-
-  if (userIdPresent) {
-    req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  }
-
-  let putItemParams = {
-    TableName: tableName,
-    Item: req.body
-  }
-  dynamodb.put(putItemParams, (err, data) => {
-    if(err) {
-      res.statusCode = 500;
-      res.json({error: err, url: req.url, body: req.body});
-    } else{
-      res.json({success: 'post call succeed!', url: req.url, data: data})
+      res.json({ error: err, url: req.url, body: req.body });
+    } else {
+      res.json({ data: item });
     }
   });
 });
 
 /**************************************
-* HTTP remove method to delete object *
-***************************************/
+ * HTTP remove method to delete object *
+ ***************************************/
 
-app.delete(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
-  var params = {};
-  if (userIdPresent && req.apiGateway) {
-    params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  } else {
-    params[partitionKeyName] = req.params[partitionKeyName];
-     try {
-      params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-  if (hasSortKey) {
-    try {
-      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
+app.delete(path + '/:itemId', function(req, res) {
+  const { itemId } = req.params;
 
-  let removeItemParams = {
+  const queryParams = {
     TableName: tableName,
-    Key: params
-  }
-  dynamodb.delete(removeItemParams, (err, data)=> {
-    if(err) {
+    Key: {
+      itemId: itemId,
+    },
+  };
+
+  dynamodb.delete(queryParams, (err, data) => {
+    if (err) {
       res.statusCode = 500;
-      res.json({error: err, url: req.url});
+      res.json({ error: err, url: req.url });
     } else {
-      res.json({url: req.url, data: data});
+      res.json({ data: { itemId } });
     }
   });
 });
+
 app.listen(3000, function() {
-    console.log("App started")
+  console.log('App started');
 });
 
-// Export the app object. When executing the application local this does nothing. However,
-// to port it to AWS Lambda we will create a wrapper around that will load the app from
-// this file
-module.exports = app
+module.exports = app;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
+import { API } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
+import faker from 'faker';
 import './App.css';
-
 class App extends Component {
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  callAPI = async () => {
+    try {
+      const create = await API.post('apia', '/items', { body: { itemName: faker.commerce.productName() } });
+      console.log('TCL: App -> callAPI -> create', create);
+
+      const getAll = await API.get('apia', '/items');
+      console.log('TCL: App -> callAPI -> getAll', getAll);
+
+      const item = getAll.data.pop();
+      console.log('TCL: App -> callAPI -> item', item);
+
+      const getById = await API.get('apia', `/items/${item.itemId}`);
+      console.log('TCL: App -> callAPI -> getById', getById);
+
+      const update = await API.put('apia', '/items', {
+        body: {
+          ...item,
+          itemName: faker.commerce.productName(),
+          price: faker.commerce.price(),
+          productMaterial: faker.commerce.productMaterial(),
+        },
+      });
+      console.log('TCL: App -> callAPI -> update', update);
+
+      const remove = await API.del('apia', `/items/${item.itemId}`);
+      console.log('TCL: App -> callAPI -> remove', remove);
+    } catch (err) {
+      console.error({ message: err });
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>aws_amplify_env</h1>
+          <h1>aws_amplify_queries</h1>
         </header>
       </div>
     );
